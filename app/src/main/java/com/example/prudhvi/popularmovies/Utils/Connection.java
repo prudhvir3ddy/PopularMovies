cec6ab654807f6d91099d0bfb35d8e289250f378
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2018-06-03 00:13:53 +0530

    final commit

--- app/src/main/java/com/example/prudhvi/popularmovies/Utils/Connection.java
+++ app/src/main/java/com/example/prudhvi/popularmovies/Utils/Connection.java
@@ -18,8 +18,8 @@ public class Connection {
         if (manager != null) {
             NetworkInfo[] info = manager.getAllNetworkInfo();
             if (info != null)
-                for (int i = 0; i < info.length; i++)
-                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                for (NetworkInfo anInfo : info)
+                    if (anInfo.getState() == NetworkInfo.State.CONNECTED)
                         return true;
         }
         return false;
